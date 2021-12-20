2606330c075a3ccf6c54e050f5ad13838c8f6a68
nickyxxxl <nickyswinckels@yahoo.com>
2021-12-20 11:44:15 +0100

    fix localstorage object

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,37 @@
 <script lang="ts">
 	import { AuthenticatePasswd, ParseForm } from "./Login.svelte";
-	
-	let curUUID = "b6256752-bbcf-42e0-8c7c-9e4643f0e827"
-	let token: object
-	let loggedIn: boolean = false;
-	// const endpoint = 'https://nickyxxxl.nl/api/v1/auth'
-	const endpoint = 'http://localhost:3000/auth'
 
-	interface loginForm {
+	interface LoginForm {
     	username:string,
     	password:string,
     	save:boolean
 	}
 
+	interface Token {
+    	access_token: string,
+    	refresh_token: string,
+    	api_url: string,
+    	expires_in: number
+	}
+	
+	let curUUID = "b6256752-bbcf-42e0-8c7c-9e4643f0e827"
+	let token: Token | void 
+	let loggedIn: boolean = false
+	let errored = false
+	const endpoint = 'https://nickyxxxl.nl/api/som/v1/auth'
+	const redirect = 'http://nickyxxxl.nl/som/main'
+
 	async function handleUnsafeLogin(target) {
-		const form:loginForm = ParseForm(target)
-		token = await AuthenticatePasswd(endpoint, form.username, form.password, curUUID)
-		loggedIn = true;
+		const form:LoginForm = ParseForm(target)
+		token = await AuthenticatePasswd(endpoint, form.username, form.password, curUUID).catch(() => { errored = true })
+		if (typeof token != 'undefined' && typeof token.access_token == 'undefined') {
+			errored = true
+		} else {
+			Object.keys(token).forEach(key => {
+				window.localStorage.setItem(key, token[key])
+			})
+			loggedIn = true
+		}
 		return
 	}
 
@@ -43,16 +58,20 @@
 		<input class="button-outline" type="submit" value="Login">
 	</fieldset></form>
 </div>
-{:else}
+{:else if !errored}
 
 	<p>Logged in!</p>
-	<p>Token: {JSON.stringify(token)}</p>
+	<p>Redirecting...</p>
+	{window.location.href = redirect}
 
+{/if}
+{#if errored}
+	<h1 class="error">ERROR!!!</h1>
+	<h4 class="error">Shit's broken (see developer console for details)</h4>
 {/if}
 </main>
 
 <style>
-
 .center{
 	transform: translate(50%,50%);
 	max-width: 50%;
@@ -61,6 +80,10 @@
 input[disabled], label[disabled]{
 	cursor: not-allowed;
 	opacity: 50%;
+}
+.error{
+	color: red;
+	font-weight: bolder;
 }
 </style>
 
--- src/Login.svelte
+++ src/Login.svelte
@@ -1,16 +1,23 @@
 <script lang="ts" context="module">
 
-interface loginForm {
+interface LoginForm {
     username:string,
     password:string,
     save:boolean
+}
+
+interface Token {
+	access_token: string,
+	refresh_token: string,
+	api_url: string,
+	expires_in: number
 }
 
 export async function AuthenticateSafe(username:string, password:string) {
     throw new Error("Function not implemented");
 }
 
-export async function AuthenticatePasswd(endpoint:string, username:string, password:string, schoolUUID:string):Promise<object>{
+export async function AuthenticatePasswd(endpoint:string, username:string, password:string, schoolUUID:string):Promise<Token>{
     const response = await fetch(endpoint, {
     	method: 'POST',
     	body: JSON.stringify({username, password, schoolUUID}),
@@ -30,7 +37,7 @@ export async function AuthenticatePasswd(endpoint:string, username:string, passw
 }
 
 //utils
-export function ParseForm(form):loginForm {
+export function ParseForm(form):LoginForm {
 	const formData = new FormData(form.target);
 	//typescript thing
 	let result = {
@@ -38,7 +45,7 @@ export function ParseForm(form):loginForm {
 		password: undefined,
 		save: undefined
 	};
-	for (let field of formData) {
+	for (const field of formData) {
 		const [key, value] = field;
 		result[key] = value;
 	}
